344753770bbd5ff73271c759d468f085f4815fdf
易立 <lethe30003000@gmail.com>
2017-02-14 18:46:55 +0800

    add setup info

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='colortable',
+    py_modules=['colortable'],
+    version='0.2',
+    description='Print colorful(256) tables for terminal with built-in themes',
+    author='lethe3000',
+    author_email='lethe30003000@gmail.com',
+    url='https://github.com/lethe3000/ctable',
+    download_url='https://github.com/lethe3000/ctable/tarball/0.2',
+    keywords=['table', 'terminal', 'color'],
+    classifiers=[],
+    install_requires=['tabulate==0.7.7'],
+)
